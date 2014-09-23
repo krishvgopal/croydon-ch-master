6490c5dfbcd8e8bbacc71f30d64ea5bdf1fed2b0
Alex Hitchins <alex@alexhitchins.com>
2014-09-23 21:16:03 +0100

    Removed unused using statements (as per resharper)

--- CollectionHubWeb/Index.aspx.cs
+++ CollectionHubWeb/Index.aspx.cs
@@ -1,10 +1,5 @@
 ﻿using CollectionHubData;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class Index : System.Web.UI.Page
 {
