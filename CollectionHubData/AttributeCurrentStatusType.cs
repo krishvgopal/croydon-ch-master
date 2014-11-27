9f6177c0af5e780b99d831065ab07a037f9392ce
alex@hitchinsit.co.uk <alex@hitchinsit.co.uk>
2014-11-27 21:14:56 +0000

    Added previously excluded files

---
+++ CollectionHubData/AttributeCurrentStatusType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionHubData
+{
+    public class AttributeCurrentStatusType
+    {
+        public int      AttributeTypeId { get; set; }
+        public string   TypeName        { get; set; }
+
+        public AttributeCurrentStatusType() { }
+        public AttributeCurrentStatusType(System.Data.SqlClient.SqlDataReader value)
+        {
+            AttributeTypeId = Convert.ToInt32(value["AttributeTypeId"]);
+            TypeName        = value["TypeName"].ToString();
+        }
+    }
+}
