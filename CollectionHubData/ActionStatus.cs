bf04449da11d81318c4746897679bd55c6bd4cfc
alex@hitchinsit.co.uk <alex@hitchinsit.co.uk>
2015-01-13 09:43:01 +0000

    Added ActionStatuses

---
+++ CollectionHubData/ActionStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace CollectionHubData
+{
+    public class ActionStatus
+    {
+        public int      ActionStatusId      { get; set; }
+        public string   ActionStatusText    { get; set; }
+
+        public ActionStatus() { }
+        public ActionStatus(IDataReader value)
+        {
+            ActionStatusId = Convert.ToInt32(value["ActionStatusId"]);
+            ActionStatusText = value["ActionStatusText"].ToString();
+        }
+    }
+}
